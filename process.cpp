ee15a181726fcd7b1a0d4c738e61c1daff756461
Tony Su <peitaosu@163.com>
2018-03-20 23:04:08 +0800

    support load process.json

--- process.cpp
+++ process.cpp
@@ -2,5 +2,17 @@
 
 Process::Process(QObject *parent) : QObject(parent)
 {
+    //load the process.json
+    QString process_string;
+    QFile process_file;
+    process_file.setFileName("process.json");
+    //set QIODevice RO and read as text
+    process_file.open(QIODevice::ReadOnly | QIODevice::Text);
+    process_string = process_file.readAll();
+    process_file.close();
+
+    //convert file to variant map
+    QJsonDocument process_json = QJsonDocument::fromJson(process_string.toUtf8());
+    process_config = process_json.object();
 
 }
--- process.h
+++ process.h
@@ -2,12 +2,18 @@
 #define PROCESS_H
 
 #include <QObject>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 class Process : public QObject
 {
         Q_OBJECT
     public:
         explicit Process(QObject *parent = 0);
+
+    private:
+        QJsonObject process_config;
 
     signals:
 
